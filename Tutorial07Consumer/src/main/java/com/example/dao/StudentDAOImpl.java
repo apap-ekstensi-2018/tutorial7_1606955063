526bd7cf7056e7d12411303443df560fe110e75b
windiany <windianysitorus@gmail.com>
2018-04-14 23:00:55 +0700

    Added tutorial07

---
+++ Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -0,0 +1,29 @@
+package com.example.dao;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.model.StudentModel;
+
+@Service
+public class StudentDAOImpl implements StudentDAO{
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Override
+	public StudentModel selectStudent(String npm) {
+		StudentModel student = restTemplate.getForObject("http://localhost:8080/rest/student/view/"+npm, StudentModel.class);
+		return student;
+	}
+	
+	@Override
+	public List<StudentModel> selectAllStudents(){
+		StudentModel[] students = restTemplate.getForObject("http://localhost:8080/rest/student/viewall", StudentModel[].class);
+		List<StudentModel> list_student = Arrays.asList(students);
+		return list_student;
+	}
+}
